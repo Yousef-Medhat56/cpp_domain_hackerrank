35f1fdab029a0d4f81ae42c2f7c29d9f8dd6156e
Yousef Medhat <yousef.medhat564@gmail.com>
2022-10-06 15:07:40 +0200

    Create input_and_output.cpp

---
+++ introduction/input_and_output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // declare variables
+    int x,y,z;
+    
+    // get the input from the user
+    cin >> x >> y >> z; 
+    
+    // calculate the sum of variables
+    int sum = x+y+z;
+    
+    // print the sum
+    cout << sum;
+    return 0;
+}
